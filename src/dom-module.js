83664dc47f4b93be1f27ab8d7cafa631e2213abb
Ken <knetngyn@gmail.com>
2022-08-06 16:06:05 -0500

    Add input for tasks and start DOM manipulation

---
+++ src/dom-module.js
@@ -0,0 +1,9 @@
+const projectContainer = document.querySelector(".project-container");
+const mainContainer = document.querySelector(".main-container");
+const projectsBtn = document.querySelector(".projects-btn");
+const addProjectInput = document.querySelector(".add-project-input");
+const addProjectBtn = document.querySelector(".add-project-btn");
+const cancelProjectBtn = document.querySelector(".cancel-project-btn");
+const addTaskBtn = document.querySelector(".add-task-btn");
+
+console.log("fart");
